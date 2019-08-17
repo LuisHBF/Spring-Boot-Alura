3dbf88af8aeef4eed3d6edae8da9254621afe391
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-17 16:03:50 -0300

    Adicionando biblioteca do Spring Data JPA, configurando acesso ao banco de dados no arquivo application.properties e mapeando entidades.

--- src/main/java/com/br/luishbf/springboot/model/Resposta.java
+++ src/main/java/com/br/luishbf/springboot/model/Resposta.java
@@ -3,12 +3,23 @@ package com.br.luishbf.springboot.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Resposta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
