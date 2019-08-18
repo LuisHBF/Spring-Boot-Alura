5229d6ac812841a3e1ddeec8dc34e422feca1c43
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-18 15:31:23 -0300

    Criando ExceptionHandler para capturar exceptions de validação do Bean Validation e retornar uma mensagem de erro mais amigável com o status code 400

---
+++ src/main/java/com/br/luishbf/springboot/config/validacao/ErroDeValidacaoHandler.java
@@ -0,0 +1,36 @@
+package com.br.luishbf.springboot.config.validacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErroDeValidacaoHandler {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<ErroDeFormularioDTO> handle(MethodArgumentNotValidException exception) {
+		List<ErroDeFormularioDTO> dto = new ArrayList<>();
+		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+		
+		fieldErrors.forEach(e -> {
+			String mensagem = this.messageSource.getMessage(e, LocaleContextHolder.getLocale());
+			dto.add(new ErroDeFormularioDTO(e.getField(), mensagem));
+		});
+		
+		return dto;
+	}
+	
+}
