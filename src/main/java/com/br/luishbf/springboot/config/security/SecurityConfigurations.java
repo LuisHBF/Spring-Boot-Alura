1520063c6398c795b1935884f3ffca8ce1b3832b
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-20 02:07:05 -0300

    Criação da classe responsável por gerenciar a segurança da aplicação

---
+++ src/main/java/com/br/luishbf/springboot/config/security/SecurityConfigurations.java
@@ -0,0 +1,12 @@
+package com.br.luishbf.springboot.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+
+	
+}
