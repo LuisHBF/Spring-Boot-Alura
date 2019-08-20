2acb7440035c1a4325e0aedfe1aa4f0104c1e1e7
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-20 02:16:28 -0300

    Liberando acesso a endpoitns publicos

--- src/main/java/com/br/luishbf/springboot/config/security/SecurityConfigurations.java
+++ src/main/java/com/br/luishbf/springboot/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package com.br.luishbf.springboot.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +12,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 
+	//Configurações de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
 	
+	//Configurações de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+	
+	//Configurações de recursos estaticos(js,css,img,etc..)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+	}
 }
