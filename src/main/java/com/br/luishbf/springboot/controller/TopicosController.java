b3d097db21b069711e4dfa1f0325ad51d68c1f13
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-19 22:08:49 -0300

    adicionando paginação de registros por parametros

--- src/main/java/com/br/luishbf/springboot/controller/TopicosController.java
+++ src/main/java/com/br/luishbf/springboot/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,8 +43,8 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDTO> listar(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd){
-		  Pageable pageable = PageRequest.of(pagina, qtd);
+	public Page<TopicoDTO> listar(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd, @RequestParam String filtro){
+		  Pageable pageable = PageRequest.of(pagina, qtd,Direction.DESC, filtro);
 		  Page<Topico> topicos = nomeCurso == null ? this.topicoRepository.findAll(pageable) : this.topicoRepository.findByCursoNome(nomeCurso, pageable);
 		  return TopicoDTO.converter(topicos);
 	}
