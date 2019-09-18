f84ce1b64d402a60028f94cdc93cc9d6cbd393eb
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-09-18 16:51:45 -0300

    Criando filtro para recuperar token da requisição

--- src/main/java/com/br/luishbf/springboot/config/security/SecurityConfigurations.java
+++ src/main/java/com/br/luishbf/springboot/config/security/SecurityConfigurations.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -41,7 +42,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.anyRequest().authenticated()
 		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new TokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	//Configurações de recursos estaticos(js,css,img,etc..)
