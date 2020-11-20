602e7f6b8996f948684024d9ae0576366db204f2
Luis Henrique B. Franco <luishbfranco@gmail.com>
2020-11-20 12:58:43 -0300

    projeto base

---
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -0,0 +1,17 @@
+package br.com.alura.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableCaching
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
